c9bdbc837cec80b780272a4a45c74aadb2540055
YOUSSEF ABIR <youssefabir891@gmail.com>
2026-05-18 15:50:37 +0100

    component.js

--- src/App.js
+++ src/App.js
@@ -1,18 +1,17 @@
 import Name from './Name';
-import price from './price';
-import description from './description';
-import image from './image';
-import product from './product';
+import Price from './price';
+import Description from './description';
+import Image from './image';
+
 
 
 function App() {
   return (
     <>
       <Name />
-      <price />
-      <description />
-      <image />
-      <product />
+      <Price />
+      <Description />
+      <Image />
     </>
   );
 }
