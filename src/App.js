47c0176d5edf15cbf5e94ad2311ed8d1762e78d2
YOUSSEF ABIR <youssefabir891@gmail.com>
2026-05-21 21:22:38 +0100

    component.js

--- src/App.js
+++ src/App.js
@@ -1,17 +1,31 @@
 import Name from './Name';
 import Price from './price';
 import Description from './description';
-import Image from './image';
+
+import Button from 'react-bootstrap/Button';
+import Card from 'react-bootstrap/Card';
 
 
 
 function App() {
   return (
     <>
-      <Name />
-      <Price />
-      <Description />
-      <Image />
+     <Card style={{ width: '18rem' }}>
+      <Card.Img variant="top" src="/Theramag375.png" />
+      <Card.Body>
+        <Name />
+        <Description />
+        <Price />
+        <Button variant="primary">Add to Card</Button>
+      </Card.Body>
+    </Card>
+
+    <div style={{ textAlign : 'center', fontFamily: 'times new roman', fontSize: '4em', color: '#c70404' }}>
+    <p>
+      <strong>Hello, there!</strong>
+    </p>
+    </div>
+
     </>
   );
 }
--- src/product.js
+++ src/product.js
@@ -2,7 +2,7 @@ const product = {
     name: "THERAMAG 375 mg",
     price: 9.25,
     description: "THERAMAG 375 mg est un médicament utilisé pour traiter les carences en magnésium. Il contient du magnésium sous forme de citrate, qui est facilement absorbé par l'organisme. Ce médicament est souvent prescrit pour soulager les symptômes liés à une carence en magnésium, tels que les crampes musculaires, la fatigue et les troubles du sommeil.",
-    image: "https://therapharm.ma/product.php?id=THERAMAG-375"
+    image: "https://therapharm.ma/uploads/Produits/Theramag375.png"
 }
 
 export default product;
